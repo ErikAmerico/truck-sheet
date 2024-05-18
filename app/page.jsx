47631282022787ea2fa615c0ef481067aa700753
Erik Olson <erikamerico1991@gmail.com>
2024-05-18 14:19:49 -0400

    Setup global Prisma client instance
    - Create a new file `lib/prisma.js` to setup a global Prisma client instance
      to prevent excessive connections to the database
    - check the global object `global.prisma` to see if the client instance is
      already created
    - If the client instance is not created, create a new client instance
    - Export the client instance
    
    I am committing the changes in page.jsx just for future
    reference. The changes in page.jsx are not related to the
    Prisma client instance setup.

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+// use the global prisma instance defined in lib/prisma.js
+// instead of  import { PrismaClient } from '@prisma/client';
+// which would create a new instance of PrismaClient
+import { prisma } from "../lib/prisma";
+
+export default async function Page() {
+  // just testing out the usage of prisma in a page
+  const employee = await prisma.employee.findUnique({
+    where: {
+      // usernames are marked as unique in the schema
+      // so using findUnique is safe
+      username: "chrisdas",
+    },
+  });
+
+  console.log({ employee });
+  // use the main as the outermost tag
+  return <main>Hello, {employee?.firstName}</main>;
 }
