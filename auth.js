04b8f9e19b6cab4ae55cd04f1438edc50f1e6892
Erik Olson <erikamerico1991@gmail.com>
2024-05-23 00:21:05 -0400

    feat(auth): Implement authentication with NextAuth    and Prisma
    - Added NextAuth configuration with credentials provider
    - Integrated Prisma for database interactions
    - Implemented user authentication logic
    - Included password validation using bcrypt

--- auth.js
+++ auth.js
@@ -1,5 +1,47 @@
-import NextAuth from "next-auth"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { comparePassword } from "./utils/saltHashPw";
+import prisma from "./lib/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        console.log("Authorize called with credentials:", credentials);
+
+        const { username, password } = credentials;
+
+        try {
+          const employee = await prisma.employee.findUnique({
+            where: {
+              username,
+            },
+          });
+
+          if (!employee) {
+            throw new Error("Employee not found.");
+          }
+
+          const isPasswordValid = await comparePassword(
+            password,
+            employee.password
+          );
+          if (!isPasswordValid) {
+            throw new Error("Invalid password.");
+          }
+
+          return employee;
+        } catch (error) {
+          console.error("Error finding employee", error);
+          throw new Error(
+            "Authentication failed. Please check your credentials and try again."
+          );
+        }
+      },
+    }),
+  ],
+});
