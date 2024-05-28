5a8d9f56e550774327d8103bc8725a0e8a072eae
Erik Olson <erikamerico1991@gmail.com>
2024-05-28 03:05:04 -0400

    added video as background to login page, styled login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "auth";
 import { redirect } from "next/navigation";
 import { LoginForm } from "./form";
+import "./page.css";
 
 export default async function LoginPage() {
   const session = await auth();
@@ -10,9 +11,12 @@ export default async function LoginPage() {
   }
 
   return (
-    <div>
-      <div>
-        <h1>Login</h1>
+    <div id="login-page-container">
+      <video autoPlay muted loop id="login-page-video">
+        <source src="/Trucks.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div id="login-form-wrapper">
         <LoginForm />
       </div>
     </div>
