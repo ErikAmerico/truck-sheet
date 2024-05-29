9df233214a20677b907c9f59da8735c2f69b0ce5
Erik Olson <erikamerico1991@gmail.com>
2024-05-28 21:55:16 -0400

    create mvp appbar with styling

--- app/components/appbar.tsx
+++ app/components/appbar.tsx
@@ -1,29 +1,43 @@
-import React from "react";
-import { auth, signOut, signIn } from "auth";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Image from "next/image";
+import { auth, signOut } from "auth";
+import "./appbar.css";
 
-async function Appbar() {
+export default async function NavBar() {
   const session = await auth();
+
+  if (!session || !session.user) {
+    return null;
+  }
+
   return (
-    <div className="">
-      <div className="">
-        {session && session.user ? (
-          <div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" id="appbar">
+        <Toolbar id="toolbar">
+          <Image src="/logo.png" alt="Logo" width={140} height={60} />
+          <Box sx={{ flexGrow: 1 }} />
+          <Box id="user-info-container">
+            <Typography variant="h6" component="div" id="first-last-name">
+              {session.user.firstName} {session.user.lastName}
+            </Typography>
             <form
               action={async () => {
                 "use server";
                 await signOut();
               }}
             >
-              {session.user.username}
-              <button type="submit">Sign out</button>
+              <Button type="submit" id="logout-button">
+                Logout
+              </Button>
             </form>
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default Appbar;
