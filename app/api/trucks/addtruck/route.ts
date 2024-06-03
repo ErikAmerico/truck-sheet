3ed1eb18adf3fc5a7ae00c9a3f77abc0738bf00e
Erik Olson <erikamerico1991@gmail.com>
2024-06-02 21:22:29 -0400

    add truck eror handling and success message
    -When adding a new truck with the same truck number
    as an existing truck, the user is now shown an error
    and allowed to retry with a different truck number.
    
    -When adding a new truck with an available truck number,
    the user is shown a success message and the truck is
    added to the database.

--- app/api/trucks/addtruck/route.ts
+++ app/api/trucks/addtruck/route.ts
@@ -5,6 +5,17 @@ export async function POST(req: NextRequest) {
   const { number } = await req.json();
 
   try {
+    const existingTruck = await prisma.truck.findUnique({
+      where: { number },
+    });
+
+    if (existingTruck) {
+      return NextResponse.json(
+        { error: "Truck number already exists" },
+        { status: 400 }
+      );
+    }
+
     const newTruck = await prisma.truck.create({
       data: {
         number,
