109b25fd012dc95285ba30e9331a2b508e4930f7
Erik Olson <erikamerico1991@gmail.com>
2025-03-10 15:30:59 -0400

    check if user or truck exists before seeding database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,29 +3,40 @@ const prisma = new PrismaClient();
 import { saltAndHashPassword } from "../utils/saltHashPw.js";
 
 async function main() {
-  const hashedPassword = await saltAndHashPassword("Chris123.");
-  const employee = await prisma.employee.upsert({
+  const existingEmployee = await prisma.employee.findUnique({
     where: { username: "chrisdas" },
-    update: {},
-    create: {
-      role: "office",
-      username: "chrisdas",
-      password: hashedPassword,
-      firstName: "Chris",
-      lastName: "Das",
-    },
   });
+  //check if employee exists before seeding
+  if (!existingEmployee) {
+    const hashedPassword = await saltAndHashPassword("Chris123.");
+    const employee = await prisma.employee.create({
+      data: {
+        role: "office",
+        username: "chrisdas",
+        password: hashedPassword,
+        firstName: "Chris",
+        lastName: "Das",
+      },
+    });
+    console.log("Employee seeded: ", employee);
+  } else {
+    console.log("Employee already exists");
+  }
 
-  const truck = await prisma.truck.upsert({
+  const existingTruck = await prisma.truck.findUnique({
     where: { number: 100 },
-    update: {},
-    create: {
-      number: 100,
-    },
   });
-
-  console.log({ employee });
-  console.log({ truck });
+  //check if truck exists before seeding
+  if (!existingTruck) {
+    const truck = await prisma.truck.create({
+      data: {
+        number: 100,
+      },
+    });
+    console.log("Truck created:", truck);
+  } else {
+    console.log("Truck already exists, skipping creation.");
+  }
 }
 
 main()
